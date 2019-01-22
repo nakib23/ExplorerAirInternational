734ceae468aeca499a39fd54b3acb7ccf2c8f669
nakib23 <nakib23@outlook.com>
2019-01-22 22:59:36 +0600

    Initial Commit

---
+++ app/src/main/java/com/example/connection/explorerairinternational/MainActivity.java
@@ -0,0 +1,19 @@
+package com.example.connection.explorerairinternational;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button travelButton, recruitButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    onclicklistener
+}
---
+++ app/src/main/java/com/example/connection/explorerairinternational/RecruitActivity.java
@@ -0,0 +1,13 @@
+package com.example.connection.explorerairinternational;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RecruitActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recruit);
+    }
+}
