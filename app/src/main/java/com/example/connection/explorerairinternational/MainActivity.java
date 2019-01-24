1e523efa95ba9281e88e2cc045c33a0af45517cd
nakib23 <nakib23@outlook.com>
2019-01-25 03:27:59 +0600

    Changed background, Changed button design, Set Onclicklistener in main activity

--- app/src/main/java/com/example/connection/explorerairinternational/MainActivity.java
+++ app/src/main/java/com/example/connection/explorerairinternational/MainActivity.java
@@ -1,19 +1,47 @@
 package com.example.connection.explorerairinternational;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
 
 
     Button travelButton, recruitButton;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        travelButton.findViewById(R.id.mTravelBtn);
+        recruitButton.findViewById(R.id.mRecruitBtn);
+
+   travelButton.setOnClickListener(new View.OnClickListener() {
+       @Override
+       public void onClick(View v)
+       {
+
+           Intent i = new Intent(MainActivity.this,TravelActivity.class);
+           startActivity(i);
+       }
+   });
+
+        recruitButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+
+                Intent i = new Intent(MainActivity.this,RecruitActivity.class);
+                startActivity(i);
+            }
+        });
+
     }
 
-    onclicklistener
 }
